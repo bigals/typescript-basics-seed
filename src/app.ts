34b0b4e0b6b57b1999ae41d4806239cc82483895
bigals <alexander.wil.mclean@gmail.com>
2018-02-21 13:55:38 -0500

    end of object literal improvements

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,29 @@
-function multiply(a: any, b = 25) {
-  return a * b;
+interface Pizza {
+    name: string;
+    price: number;
+    getName(): string
+}
+interface Order {
+    pizza: Pizza,
+    toppings: string[]
 }
 
-console.log(multiply(5));
-console.log(multiply(5, 35));
+const pizza:Pizza = {
+    name: 'Pepperoni',
+    price: 15,
+    getName: (): string => {
+        return pizza.name;
+    }
+};
+
+
+const toppings: string[] = [ 'pepperoni' ];
+const order: Order = { pizza, toppings };
+
+function createOrder(pizza: Pizza, toppings: string[]) {
+    return { pizza, toppings };
+}
+
+console.log(order);
+console.log(createOrder(pizza, toppings));
+
