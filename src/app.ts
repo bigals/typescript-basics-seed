60bfd595c8c0d66d8c02f6c0b668ead76b8b0915
bigals <alexander.wil.mclean@gmail.com>
2018-02-21 14:48:26 -0500

    end of array and object spreads

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
-function sumAll(message: any, ...arr: any[]) {
-    console.log(arguments);
-    console.log(
-        '---------------------------'
-    );
-    console.log(message);
-    return arr.reduce((prev: any, next:any ) => prev + next );
-}
+const toppings = [ 'bacon', 'chilli' ];
 
-const sum = sumAll('HELLO',1, 2, 3, 4, 5, 6, 7, 8, 9, 10 );
-console.log(sum);
+const newToppings = [ 'pepperoni' ];
+
+const allToppings = [ ...toppings, ...newToppings ];
+
+console.log(allToppings);
