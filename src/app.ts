27b210c2e0f68cb294eb1924d89910228d2ac15b
bigals <alexander.wil.mclean@gmail.com>
2018-02-25 09:38:33 -0500

    end of union and literal types

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
-let coupon: string | null = 'pizza25';
+let pizzaSize: string = 'small';
 
-function removeCoupon(): void {
-    coupon = null;
+function selectSize(size: 'small' | 'medium' | 'large'): void {
+    pizzaSize = size;
 }
 
-console.log(coupon);
+console.log(`Pizza size before selection: ${ pizzaSize }`);
 
-removeCoupon();
+selectSize('large');
 
-console.log(coupon);
+console.log(`Pizza size after selection: ${ pizzaSize }`);
+
+
+//Example 2
+let pizzaSize2: number = 1;
+
+function selectSize2(size: 1 | 2 | 3): void {
+    pizzaSize2 = size;
+}
+
+console.log(`Pizza size before selection: ${ pizzaSize2 }`);
+
+selectSize2(2);
+
+console.log(`Pizza size after selection: ${ pizzaSize2 }`);
