0b89325d1466f2c4c12fcea105adfed65ddecc08
bigals <alexander.wil.mclean@gmail.com>
2018-02-25 10:00:40 -0500

    end of typed functions and defaults

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,7 @@
 
 let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y) => {
-    if (y) {
-        return x * y
-    }
-    return x;
-};
-
+sumOrder = (x, y = 1) => x * y;
 
 const sum = sumOrder(25);
 sumOrder(25, 2);
