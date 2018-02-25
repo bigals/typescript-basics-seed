30fa309faa2d6e3c3c6cd127da71cbf179fbb5e0
bigals <alexander.wil.mclean@gmail.com>
2018-02-25 09:57:34 -0500

    end of functions and optional arguments

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 
 
-let sumOrder: (price: number, quantity: number) => number;
+let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y) => x * y;
+sumOrder = (x, y) => {
+    if (y) {
+        return x * y
+    }
+    return x;
+};
 
 
-const sum = sumOrder(25, 2);
+const sum = sumOrder(25);
+sumOrder(25, 2);
 
 console.log(`Total sum Named: ${ sum }`);
