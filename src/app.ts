959a4ba27f9953eac8abd5f8276d119ae44cba76
bigals <alexander.wil.mclean@gmail.com>
2018-05-05 06:23:32 -0400

    End of Interface contracts with implements

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-class Sizes {
+interface SizesInterface {
+    availableSizes: string[];
+}
+
+abstract class Sizes implements SizesInterface {
     constructor (protected sizes: string[]) { }
 
     set availableSizes(sizes: string[]) {
@@ -9,7 +13,15 @@ class Sizes {
     }
 }
 
-class Pizza extends Sizes {
+interface PizzaInterface extends SizesInterface {
+    readonly name: string;
+    toppings: string[];
+    updateSizes(sizes: string[]): void;
+    addTopping(topping: string): void;
+
+}
+
+class Pizza extends Sizes implements PizzaInterface {
     public toppings: string[] = [];
     constructor (readonly name: string, sizes: string[]) {
         super(sizes);
