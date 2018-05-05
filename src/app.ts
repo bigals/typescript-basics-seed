715609d0239449b4f2efdd71f42d7ab2dde19278
bigals <alexander.wil.mclean@gmail.com>
2018-05-05 06:00:34 -0400

    End of setters/Getters (Accessors)

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
+class Sizes {
+    constructor (public sizes: string[]) { }
+
+    set availableSizes(sizes: string[]) {
+        this.sizes = sizes;
+    }
+    get availableSizes() {
+        return this.sizes;
+    }
+}
+
+const sizes = new Sizes([ 'small', 'medium' ]);
+
+//invoke getter
+console.log(sizes.availableSizes);
+
+//invoke setter
+sizes.availableSizes = [ 'medium', 'large' ];
+console.log(sizes.availableSizes);
+
 class Pizza {
     constructor (readonly name: string) {}
     public toppings: string[] = [];
@@ -10,6 +30,5 @@ class Pizza {
 const pizza = new Pizza('Pepperoni');
 
 pizza.addTopping('Pepperoni');
-
 
 console.log(pizza);
